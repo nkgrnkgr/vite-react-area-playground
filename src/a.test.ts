533ccd0686e92127244e9786a6816becd7157d4d
nkgrnkgr <beefupme@gmail.com>
2024-06-08 20:17:45 +0900

    add test

---
+++ src/a.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("a", () => {
+  test("a", () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/b.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, test } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  test("App test", async () => {
+    render(<App />);
+    const user = userEvent.setup();
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    const text = screen.getByRole("generic", { name: "count" });
+    expect(text).toHaveTextContent("0");
+    await user.click(button);
+    expect(text).toHaveTextContent("1");
+  });
+});
