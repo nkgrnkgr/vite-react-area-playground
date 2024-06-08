9203253bfa6e16de9a796f8763955777fd5c9403
nkgrnkgr <beefupme@gmail.com>
2024-06-08 19:21:13 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Vite</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div aria-label="count">{count}</div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increment +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
